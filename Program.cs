9b595b1eb8ab56ba8f63875b689ec60803befe49
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-11 11:58:55 +0530

    goto commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string userName;
+            Again:
             Console.WriteLine("Enter User Name");
             userName = Console.ReadLine();
             if(userName.Length>=6)
@@ -20,6 +21,7 @@ namespace ConsoleApp1
             else
             {
                 Console.WriteLine("Invalid User Name");
+                goto Again;
             }
             Console.ReadKey();
         }
